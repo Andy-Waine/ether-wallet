72d4611bfbff4f2cd5d1be0b8bfde64638cd8427
A W <amwtablet@gmail.com>
2023-01-31 13:20:14 -0500

    adding bootstrap button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Button variant="secondary">
+          <img src="images/metamask.svg" alt="metamask" width="50" height ="50" />
+        </Button>
+        <div className='mt-2 mb-2'>Connected Account:</div>
+        <Button variant="danger">
+          Disconnect Metamask{' '}
+          <img src="images/hand.svg" alt="disconnet" width="50" height="50" />
+        </Button>
       </header>
     </div>
   );
